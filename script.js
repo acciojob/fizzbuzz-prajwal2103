c4308ab7f76adf27198e04253a7294250c4cd290
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-04 16:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+for (let index = 1; index <= 20; index++){
+	if (index%3==0 && index%5==0) {
+		alert("FizzBuzz")
+	}
+	else if(index%3==0){
+		alert("Fizz")
+	}
+	else if(index%5==0){
+		alert("Buzz")
+	}
+	
+}
